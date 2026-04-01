54e7e25802b94d157f737b2cf25d0c3de8e61ef6
Shreyashdhavale <shreyashdhavale2004@gmail.com>
2026-04-01 11:59:37 +0530

    changed the server file

--- src/queue.ts
+++ src/queue.ts
@@ -1,12 +1,9 @@
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
-const connection = new IORedis(
-  "rediss://default:gQAAAAAAAVz5AAIncDE3Y2U0NjMxODBlZGY0NDU3YmQxZmY3MTc3MzFmOWY3OXAxODkzMzc@humane-jennet-89337.upstash.io:6379",
-  {
-    maxRetriesPerRequest: null,
-  }
-);
+const connection = new IORedis(process.env.REDIS_URL!, {
+  maxRetriesPerRequest: null,
+});
 
 export const registrationQueue = new Queue("registration", {
   connection,
